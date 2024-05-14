3723bb09702add6bbdedf0a35f3afe9b70e9ab9a
wwxxsaa <kampers311@gmail.com>
2024-05-14 14:00:00 +0200

    Improve DApp transaction flow

--- reward_pooling.go
+++ reward_pooling.go
@@ -1,3 +1,4 @@
 // Refactor decentralized storage system
 // Refactor storage layout for better efficiency
 // Improve oracle data validation mechanisms
+// Add support for zk-SNARKs in smart contracts
