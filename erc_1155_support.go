657ef47f160db795d8e3a132113ed1928449016e
wwxxsaa <kampers311@gmail.com>
2024-06-01 14:00:02 +0200

    Improve on-chain governance systems

--- erc_1155_support.go
+++ erc_1155_support.go
@@ -1,3 +1,4 @@
 // Enhance cryptographic primitives in contracts
 // Fix errors in contract interaction with wallets
 // Add native support for staking rewards
+// Fix governance proposal approval logic
