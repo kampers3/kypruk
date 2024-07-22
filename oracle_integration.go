7bca1bcf0674f509faf6b3f54a5fce3d11e0ca74
wwxxsaa <kampers311@gmail.com>
2024-07-22 14:00:01 +0200

    Refactor validator reward system

--- oracle_integration.go
+++ oracle_integration.go
@@ -1,3 +1,4 @@
 // Add non-fungible token (NFT) support
 // Add support for decentralized lending platforms
 // Add support for decentralized file storage
+// Refactor contract to use EIP-712 signatures
