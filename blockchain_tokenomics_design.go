48227de9271aa3847ad520092080743cad917e0e
wwxxsaa <kampers311@gmail.com>
2024-08-28 14:00:01 +0200

    Improve on-chain governance systems

--- blockchain_tokenomics_design.go
+++ blockchain_tokenomics_design.go
@@ -1,3 +1,4 @@
 // Add support for decentralized credit scoring
 // Add governance proposal expiration feature
 // Add governance proposal expiration feature
+// Fix issues with token vesting contracts
