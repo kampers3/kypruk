ac111bccaa8b64de0a3bbec0806398a15fcf219a
wwxxsaa <kampers311@gmail.com>
2024-11-21 14:00:01 +0200

    Fix issues with token vesting contracts

--- escrow.py
+++ escrow.py
@@ -2,3 +2,4 @@
 // Fix errors in contract interaction with wallets
 // Integrate with external DeFi protocols
 // Fix interoperability issues with other blockchains
+// Add support for zk-SNARKs in smart contracts
